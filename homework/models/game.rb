3bbca9b52ae28d716c28c774e8f4ca91e1b8e0aa
Fiona Wilson <fionaw237@gmail.com>
2018-08-06 19:59:27 +0100

    Possible solution for further extension

--- homework/models/game.rb
+++ homework/models/game.rb
@@ -9,15 +9,37 @@ class Game
 
     hands = [@hand_1, @hand_2]
 
+    # for MVP
+    # if @hand_1 == @hand_2
+    #   return "It's a draw!"
+    # elsif hands.include?("rock") && hands.include?("paper")
+    #   return "Paper wins!"
+    # elsif hands.include?("rock") && hands.include?("scissors")
+    #   return "Rock wins!"
+    # elsif hands.include?("paper") && hands.include?("scissors")
+    #   return "Scissors wins!"
+    # end
+
+    #further extension
     if @hand_1 == @hand_2
       return "It's a draw!"
-    elsif hands.include?("rock") && hands.include?("paper")
-      return "Paper wins!"
-    elsif hands.include?("rock") && hands.include?("scissors")
-      return "Rock wins!"
-    elsif hands.include?("paper") && hands.include?("scissors")
-      return "Scissors wins!"
+    elsif player_1_wins?()
+      return "Player 1 wins by playing #{@hand_1}!"
+    else
+      return "Player 2 wins by playing #{@hand_2}!"#false
+    end
+
+  end
+
+  def player_1_wins?()
+    if (@hand_1 == "rock" && @hand_2 == "scissors") || (@hand_1 == "paper" && @hand_2 == "rock") || (@hand_1 == "scissors" && @hand_2 == "paper")
+      return true
+    else
+      return false
     end
   end
+
+
+
 
 end
