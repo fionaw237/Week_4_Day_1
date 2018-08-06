2052a443424cc285a3fcbd61b3a4238d7f385dc4
Fiona Wilson <fionaw237@gmail.com>
2018-08-06 18:21:09 +0100

    Adds welcome page and route to get to it

--- homework/controller.rb
+++ homework/controller.rb
@@ -7,20 +7,23 @@ also_reload("./models/*")
 
 
 get '/:hand1/:hand2' do
-
   game = Game.new(params[:hand1], params[:hand2])
   @outcome = game.play()
   erb(:result)
-  # if hand1 == hand2
-  #   return "It's a draw!"
-  # elsif hand1 = "rock" && hand2 == "scissors"
-  #   return "Player 1 wins!"#true
-  # elsif hand1 == "paper" && hand2 == "rock"
-  #   return "Player 1 wins!"#true
-  # elsif hand1 == "scissors" && hand2 == "paper"
-  #   return "Player 1 wins!"#true
-  # else
-  #   return "Player 2 wins!"#false
-  # end
-
 end
+
+get '/' do
+  erb(:welcome)
+end
+
+# if hand1 == hand2
+#   return "It's a draw!"
+# elsif hand1 = "rock" && hand2 == "scissors"
+#   return "Player 1 wins!"#true
+# elsif hand1 == "paper" && hand2 == "rock"
+#   return "Player 1 wins!"#true
+# elsif hand1 == "scissors" && hand2 == "paper"
+#   return "Player 1 wins!"#true
+# else
+#   return "Player 2 wins!"#false
+# end
